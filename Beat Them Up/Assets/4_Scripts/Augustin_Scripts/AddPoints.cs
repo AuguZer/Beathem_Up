748aa98460d8e9757cc5243d6e47eff46d956f5c
AuguZer <augumahieu@hotmail.fr>
2023-01-12 14:46:12 +0100

    COLLECTIBLES
    Création des animations des collectables (Green & Red Can + Vinyle et K7) se mettent à clignoter au bout de 5 secondes et sont détruits au bout de 10

--- Beat Them Up/Assets/4_Scripts/Augustin_Scripts/AddPoints.cs
+++ Beat Them Up/Assets/4_Scripts/Augustin_Scripts/AddPoints.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class AddPoints : MonoBehaviour
 {
     [SerializeField] float points = 200f;
+
+    [SerializeField] Animator collectAnimator;
+
+    [SerializeField] float collectDuration = 5f;
+    [SerializeField] float collectDestroy = 10f;
+    float t;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +20,17 @@ public class AddPoints : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        t += Time.deltaTime;
+
+        if (t > collectDuration)
+        {
+            collectAnimator.SetBool("WillDestroy", true);
+        }
+
+        if (t > collectDestroy)
+        {
+            Destroy(gameObject);
+        }
         
     }
 
